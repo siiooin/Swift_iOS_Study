058b00838437a6f38bf7b0f7b216dd470b764fcf
siiooin <sssyyy0716@naver.com>
2026-07-11 18:12:55 +0900

    끝말잇기 게임 App

---
+++ wordRelay/ForLoop.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+for i in 1...5{
+    print("안녕하세요")
+    print("아이는 = \(i)")
+}
---
+++ wordRelay/wordRelay/wordRelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordRelayApp.swift
+//  wordRelay
+//
+//  Created by 전시언 on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct wordRelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
